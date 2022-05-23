c824f256290e4cd1bb8c924039b1242e04f30423
Naif Mohamed <naifmhd@gmail.com>
2022-05-23 12:18:44 +0500

    yarn

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-export { default as PButton } from "./Button/PButton.vue";
-export { default as PTable } from "./Table/PTable.vue";
-export { default as PTextField } from "./TextField/PTextField.vue";
-export { default as PDropMenu } from "./DropMenu/PDropMenu.vue";
-export { default as PComboBox } from "./ComboBox/PComboBox.vue";
-export { default as PRadioGroup } from "./RadioGroup/PRadioGroup.vue";
-export { default as PForm } from "./Form/PForm.vue";
-export { default as PNavBar } from "./NavBar/PNavBar.vue";
+export { default as PButton } from "./Button/PButton.vue"
+export { default as PTable } from "./Table/PTable.vue"
+export { default as PTextField } from "./TextField/PTextField.vue"
+// export { default as PDropMenu } from "./DropMenu/PDropMenu.vue"
+// export { default as PComboBox } from "./ComboBox/PComboBox.vue"
+// export { default as PRadioGroup } from "./RadioGroup/PRadioGroup.vue"
+// export { default as PForm } from "./Form/PForm.vue"
+// export { default as PNavBar } from "./NavBar/PNavBar.vue"
 // export * from "./Form/TestForm";
