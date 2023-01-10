0767fd9d02ba0754e32a778b2f179b8ebe0cfe0d
Ahmed Naxeem <anazeem@gmail.com>
2023-01-10 15:11:34 +0500

    UD-24 heroicon replacement

--- src/components/PoTopBar/PoTopBar.vue
+++ src/components/PoTopBar/PoTopBar.vue
@@ -36,20 +36,7 @@
             class="block w-6 text-slate-100 sm:hidden"
             role="button"
           >
-            <svg
-              class="stroke-current"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
+          <MagnifyingGlassIcon class="stroke-current" />
           </span>
           <PoNotificationHub :notifications="notifications" />
           <PoAppTray :app-list="appList" />
@@ -66,7 +53,7 @@ export default {
 };
 </script>
 <script setup>
-import { Bars3Icon } from "@heroicons/vue/24/outline";
+import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/vue/24/outline";
 
 import {
   PoAppIcon,
