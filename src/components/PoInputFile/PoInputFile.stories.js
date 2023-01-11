367172b0da450973c5b7e6d6cdccd2a86676b0e6
Ahmed Naxeem <anazeem@gmail.com>
2023-01-11 15:26:09 +0500

    UD-24 PoInputFile

---
+++ src/components/PoInputFile/PoInputFile.stories.js
@@ -0,0 +1,67 @@
+// YourComponent.stories.js
+
+import PoInputFile from './PoInputFile.vue';
+import PoCard from '../PoCard/PoCard.vue';
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Components/Input File',
+  component: PoInputFile,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Form upload input field.'
+      }
+    },
+    backgrounds: {
+      default: 'gray',
+      values: [
+        { name: 'gray', value: '#f8fafc' },
+      ],
+    },
+  },
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => ({
+  components: { PoInputFile, PoCard },
+  setup() {
+    //👇 The args will now be passed down to the template
+    return { args };
+  },
+  template: '<PoCard class="mt-5 p-5 max-w-md"> <template v-slot:content><PoInputFile v-bind="args" /></template></PoCard>',
+});
+
+export const Normal = Template.bind({});
+export const WithMessage = Template.bind({});
+export const ErrorState = Template.bind({});
+
+Normal.args = {
+  /* 👇 The args you need here will depend on your component */
+  label: "Attache files",
+  id: 'attachment-id',
+  type: 'text'
+};
+
+WithMessage.args = {
+  /* 👇 The args you need here will depend on your component */
+  label: "Attache files",
+  id: 'attachment2-id',
+  type: 'text',
+  message: 'Attachment message.'
+
+};
+
+ErrorState.args = {
+  /* 👇 The args you need here will depend on your component */
+  label: "Attache files",
+  id: 'attachment3-id',
+  type: 'text',
+  errorMessage: 'Attachment is required.',
+  required: true
+
+};
---
+++ src/components/PoInputFile/PoInputFile.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="relative pt-5">
+        <!-- 
+            v-model update
+            @event update:modelValue
+        -->
+        <span class="text-sm font-medium text-slate-700 peer-focus:text-mpao-lightblue peer-invalid:text-red-500 peer-invalid:peer-focus:text-red-600 flex items-center space-x-1">{{ label }}</span>
+        <input
+            :name="`${id}-upload`"
+            :id="`${id}-fileupload`"
+            :value="modelValue"
+            type="file"
+            @input="$emit('update:modelValue', $event.target.value)"
+            class="sr-only peer"
+        >
+        <label :for="`${id}-fileupload`" class="mt-1 block w-full border cursor-pointer rounded-md border-slate-300 bg-white peer-focus:border-mpao-lightblue invalid:border-red-400 invalid:focus:border-red-600 invalid:focus:ring-red-600 sm:text-sm p-2">
+            <div class="flex items-center space-x-1">
+                <PaperClipIcon class="w-4 stroke-slate-500" />
+                <span class="text-sm">Choose file</span>
+            </div>
+        </label>
+
+        <p class="mt-2 text-sm text-slate-500" :id="`${id}-description`" v-if="null !== message">{{ message }}</p>
+        <p class="mt-2 text-sm text-red-600" :id="`${id}-error`" v-if="null !== errorMessage">{{ errorMessage }}</p>
+      </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "PoInputFile",
+  };
+  </script>
+  <script setup>
+  import { PaperClipIcon } from '@heroicons/vue/24/outline';
+  
+  defineProps({
+    /**
+     * Model value
+     */
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    /**
+     * Input label text
+     */
+    label: {
+      type: String,
+      default: "",
+    },
+    /**
+     * Input id text
+     */
+    id: {
+      type: String,
+      default: "",
+    },
+    /**
+     * A tool tip, helper information
+     */
+    info: {
+      type: String,
+      default: null,
+    },
+    /**
+     * Tip, description, information for the input
+     */
+    message: {
+      type: String,
+      default: null,
+    },
+    /**
+     * Error message
+     */
+    errorMessage: {
+      type: String,
+      default: null,
+    },
+  });
+  </script>
+  
