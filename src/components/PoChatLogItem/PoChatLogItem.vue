a319fbdb63628d590396d6a73e88dbcf1484d561
Ahmed Naxeem <anazeem@gmail.com>
2023-01-25 12:46:41 +0500

    UD-12 chat log component updated--- styles

--- src/components/PoChatLogItem/PoChatLogItem.vue
+++ src/components/PoChatLogItem/PoChatLogItem.vue
@@ -20,6 +20,17 @@
                             <p :class="['text-sm rounded-xl px-4 py-2', { 'bg-slate-100 text-slate-600' : 'user' !== item.userType }, { 'bg-mpao-lightblue text-sky-50' : 'user' === item.userType }]">{{ msg }}</p>
                         </div>
                     </slot>
+                    <div class="flex pt-3 flex-wrap max-w-sm">
+                        <!--
+                            Emits the button label when it’s clicked, You can listen to it and switch accordingly.
+                            @event button-click
+                        -->
+                        <button
+                        v-for="action in item.actions"
+                        @click="$emit('button-click', action.label)"
+                        class="appearance-none border border-mpao-lightblue bg-white hover:bg-mpao-lightblue px-3 py-1 mb-2 mr-2 rounded-lg text-slate-500 hover:text-sky-100 transition-colors duration-150 ease-in-out text-sm ring-0 outline-none"
+                        >{{ action.label }}</button>
+                    </div>
                 </div>
             </div>
         </div>
