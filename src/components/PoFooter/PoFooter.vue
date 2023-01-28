4d1e92bce2bfdf61cfbbde5029e7df00fec59ed1
Ahmed Naxeem <anazeem@gmail.com>
2023-01-28 21:57:48 +0500

    UD-12 footer images hover updated

--- src/components/PoFooter/PoFooter.vue
+++ src/components/PoFooter/PoFooter.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <div class="p-10 pb-5 opacity-30 hover:opacity-100 transition-opacity duration-200 ease-in-out">
+        <div class="p-10 pb-5">
             <div class="flex space-x-2 mt-5 justify-end">
-                <img class="h-10 mr-2" src="https://pension.gov.mv/asset/image/enlgish_website_logo.svg" alt="Pension Office logo">
-                <img class="h-10" src="https://pension.gov.mv/asset/image/iso_logo.jpg" alt="">
-                <img class="h-10" src="https://pension.gov.mv/asset/image/en-ccc-2020-issa.png" alt="">
+                <img class="opacity-30 hover:opacity-100 transition-opacity duration-200 ease-in-out h-10 mr-2" src="https://pension.gov.mv/asset/image/enlgish_website_logo.svg" alt="Pension Office logo">
+                <img class="opacity-30 hover:opacity-100 transition-opacity duration-200 ease-in-out h-10" src="https://pension.gov.mv/asset/image/iso_logo.jpg" alt="">
+                <img class="opacity-30 hover:opacity-100 transition-opacity duration-200 ease-in-out h-10" src="https://pension.gov.mv/asset/image/en-ccc-2020-issa.png" alt="">
             </div>
         </div>
         <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50">
