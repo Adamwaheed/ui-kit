f7a2e0ff7b51cb159ccea59abae3152862f92c92
Ahmed Naxeem <anazeem@gmail.com>
2023-02-05 14:35:16 +0500

    UD-12 fixed notificationhub, profileswitcher, apptray bug

--- dist/ui-kit.es.js
+++ dist/ui-kit.es.js
@@ -418,7 +418,7 @@ const xo = { class: "po-bg-mpao-blue po-fixed po-top-0 po-w-full po-z-50 po-flex
 }, Io = /* @__PURE__ */ s("input", {
   type: "checkbox",
   name: "",
-  class: "po-hidden peer/apps",
+  class: "po-hidden po-peer/apps",
   role: "none",
   id: "apps-menu-toggle"
 }, null, -1), Vo = { class: "po-invisible po-z-10 po-absolute -po-left-[19rem] po-top-[35px] po-opacity-0 po-bg-white po-shadow-lg po-rounded-md po-w-96 po-p-4 po-border po-border-slate-200 po-pt-5 po-space-y-5 po-transition-all po-duration-100 po-ease-linear sm:po-left-auto sm:po-right-0 peer-checked/apps:po-visible peer-checked/apps:po-top-[45px] peer-checked/apps:po-opacity-100" }, No = {
@@ -475,10 +475,10 @@ const xo = { class: "po-bg-mpao-blue po-fixed po-top-0 po-w-full po-z-50 po-flex
 }, Xo = /* @__PURE__ */ s("input", {
   type: "checkbox",
   name: "",
-  class: "po-hidden peer/apps",
+  class: "po-hidden po-peer/apps",
   role: "none",
   id: "notifications-menu-toggle"
-}, null, -1), Zo = { class: "po-invisible po-z-10 po-absolute -po-left-[19rem] po-top-[35px] po-opacity-0 po-bg-white po-shadow-lg po-rounded-md po-w-96 po-p-4 po-border po-border-slate-200 po-pt-5 po-transition-all po-duration-100 po-ease-linear sm:po-left-auto sm:po-right-0 peer-checked/apps:po-visible peer-checked/apps:po-top-[45px] peer-checked/apps:po-opacity-100" }, el = { class: "po-space-y-2 po-p-2" }, tl = { class: "po-space-y-2 po-p-2" }, ol = { class: "po-flex po-items-center po-justify-between" }, ll = { class: "po-text-sm po-font-semibold po-text-slate-700 po-grow po-flex po-space-x-2 po-items-center" }, al = {
+}, null, -1), Zo = { class: "po-invisible po-z-10 po-absolute -po-left-[19rem] po-top-[55px] po-opacity-0 po-bg-white po-shadow-lg po-rounded-md po-w-96 po-p-4 po-border po-border-slate-200 po-pt-5 po-transition-all po-duration-100 po-ease-linear sm:po-left-auto sm:po-right-[6rem] peer-checked/apps:po-visible peer-checked/apps:po-top-[64px] peer-checked/apps:po-opacity-100" }, el = { class: "po-space-y-2 po-p-2" }, tl = { class: "po-space-y-2 po-p-2" }, ol = { class: "po-flex po-items-center po-justify-between" }, ll = { class: "po-text-sm po-font-semibold po-text-slate-700 po-grow po-flex po-space-x-2 po-items-center" }, al = {
   key: 0,
   class: "w-2 h-2 rounded-full shrink-0 bg-mpao-orange"
 }, nl = { class: "grow" }, sl = { class: "po-text-xs po-font-normal po-text-slate-400 po-shrink-0" }, rl = { class: "po-block po-text-sm po-text-slate-500 po-pb-3 po-border-b po-border-slate-200" }, il = {
@@ -530,7 +530,7 @@ const xo = { class: "po-bg-mpao-blue po-fixed po-top-0 po-w-full po-z-50 po-flex
 }, cl = { class: "po-text-xs po-text-white po-font-semibold" }, fl = /* @__PURE__ */ s("input", {
   type: "checkbox",
   name: "",
-  class: "po-hidden peer/profile",
+  class: "po-hidden po-peer/profile",
   role: "none",
   id: "profile-toggle"
 }, null, -1), vl = { class: "po-invisible po-space-y-1 po-z-10 po-absolute po-right-0 po-top-[45px] po-opacity-0 po-bg-white po-shadow-lg po-rounded-md po-w-96 po-p-4 po-border po-border-slate-200 po-pt-5 po-transition-all po-duration-100 po-ease-linear peer-checked/profile:po-visible peer-checked/profile:po-top-[54px] peer-checked/profile:po-opacity-100" }, ml = ["href"], hl = { class: "po-w-5" }, bl = { class: "po-flex po-flex-col po-space-y-1" }, gl = { class: "po-text-sm po-font-normal" }, xl = {
