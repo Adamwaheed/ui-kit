3dbb8c938670296de261213c688bfcbb097b85c7
Ahmed Naxeem <anazeem@gmail.com>
2023-02-22 14:35:27 +0500

    UD-12 input file stories updated

--- src/components/PoInputFile/PoInputFile.stories.js
+++ src/components/PoInputFile/PoInputFile.stories.js
@@ -1,27 +1,25 @@
 // YourComponent.stories.js
 
-import PoInputFile from './PoInputFile.vue';
-import PoCard from '../PoCard/PoCard.vue';
+import PoInputFile from "./PoInputFile.vue";
+import PoCard from "../PoCard/PoCard.vue";
 
 //👇 This default export determines where your story goes in the story list
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Form/Input File',
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Form/Input File",
   component: PoInputFile,
   parameters: {
     docs: {
       description: {
-        component: 'Form upload input field.'
-      }
+        component: "Form upload input field.",
+      },
     },
     backgrounds: {
-      default: 'gray',
-      values: [
-        { name: 'gray', value: '#f8fafc' },
-      ],
+      default: "gray",
+      values: [{ name: "gray", value: "#f8fafc" }],
     },
   },
 };
@@ -39,29 +37,33 @@ const Template = (args) => ({
 export const Normal = Template.bind({});
 export const WithMessage = Template.bind({});
 export const ErrorState = Template.bind({});
+export const Horizontal = Template.bind({});
 
 Normal.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Attache files",
-  id: 'attachment-id',
-  type: 'text'
+  id: "attachment-id",
+  type: "text",
 };
 
 WithMessage.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Attache files",
-  id: 'attachment2-id',
-  type: 'text',
-  message: 'This is a helpful message.'
-
+  id: "attachment2-id",
+  message: "This is a helpful message.",
 };
 
 ErrorState.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Attache files",
-  id: 'attachment3-id',
-  type: 'text',
-  errorMessage: 'Attachment is required.',
-  required: true
+  id: "attachment3-id",
+  errorMessage: "Attachment is required.",
+  required: true,
+};
 
+Horizontal.args = {
+  /* 👇 The args you need here will depend on your component */
+  label: "Attache files",
+  id: "attachment4-id",
+  display: "horizontal",
 };
