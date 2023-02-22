0bd059110d85833a73bc468b7e821a0c22c7501f
Ahmed Naxeem <anazeem@gmail.com>
2023-02-22 14:32:06 +0500

    UD-12 inputfield component stories updated to make it more clear and easy to use

--- src/components/PoInputField/PoInputField.stories.js
+++ src/components/PoInputField/PoInputField.stories.js
@@ -1,29 +1,27 @@
 // YourComponent.stories.js
 
-import PoInputField from './PoInputField.vue';
+import PoInputField from "./PoInputField.vue";
 
-import PoInputFieldDocs from './PoInputFieldDocs.stories.mdx';
+import PoInputFieldDocs from "./PoInputFieldDocs.stories.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Form/Input Field',
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Form/Input Field",
   component: PoInputField,
   parameters: {
     docs: {
       page: PoInputFieldDocs,
       description: {
-        component: 'Form input field.'
-      }
+        component: "Form input field.",
+      },
     },
     backgrounds: {
-      default: 'gray',
-      values: [
-        { name: 'gray', value: '#f8fafc' },
-      ],
+      default: "gray",
+      values: [{ name: "gray", value: "#f8fafc" }],
     },
   },
 };
@@ -44,55 +42,60 @@ export const WithMessage = Template.bind({});
 export const WithPlaceholder = Template.bind({});
 export const ErrorState = Template.bind({});
 export const DisabledState = Template.bind({});
+export const Horizontal = Template.bind({});
 
 Normal.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Full name",
-  id: 'fullname-id',
-  type: 'text'
+  id: "fullname-id",
+  type: "text",
 };
 
 WithInfoTooltip.args = {
   /* 👇 The args you need here will depend on your component */
   label: "NID",
-  id: 'nid-id',
-  type: 'text',
-  info: 'National ID Card No.'
-
+  id: "nid-id",
+  type: "text",
+  info: "National ID Card No.",
 };
 
 WithMessage.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Street name",
-  id: 'fullname2-id',
-  type: 'text',
-  message: 'Fullname with middle name.'
-
+  id: "streetname",
+  type: "text",
+  message: "Name of the house, street, village",
 };
 
 WithPlaceholder.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Email address",
-  id: 'fullname2-id',
-  placeholder: 'doe@example.com',
-  type: 'tel',
+  id: "emailaddress",
+  placeholder: "doe@example.com",
+  type: "tel",
 };
 
 ErrorState.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Phone number",
-  id: 'fullname3-id',
-  type: 'email',
-  errorMessage: 'Email address is required.',
-  hasError: true
-
+  id: "phonenumber",
+  type: "text",
+  errorMessage: "Phone number is required.",
+  hasError: true,
 };
 
 DisabledState.args = {
   /* 👇 The args you need here will depend on your component */
   label: "Password",
-  id: 'fullname5-id',
-  type: 'password',
-  disabled: true
+  id: "passowrd",
+  type: "password",
+  disabled: true,
+};
 
+Horizontal.args = {
+  /* 👇 The args you need here will depend on your component */
+  label: "Short name",
+  id: "short-name",
+  display: "horizontal",
+  type: "text",
 };
