aaef6295df3cdd42bcf563a5e0e6e6b669fd56db
Ahmed Naxeem <anazeem@gmail.com>
2023-03-14 10:59:55 +0500

    UD-12 yarn install

--- dist/ui-kit.es2.js
+++ dist/ui-kit.es2.js
@@ -1,14 +1,33 @@
-function o(t) {
-  const n = new Date(t), e = {
+function i(o) {
+  const t = new Date(o), n = {
     year: "numeric",
     month: "long",
     day: "numeric"
   };
-  return n.toLocaleDateString("en-US", e);
+  return t.toLocaleDateString("en-US", n);
 }
-const a = async (t, n) => {
+const d = async (o, t) => {
+  const n = useCookie("token"), a = {
+    key: o,
+    async onRequest({ options: e }) {
+      e.headers = e.headers || {}, e.headers["Content-Type"] = "application/json", n.value && (e.headers.Authorization = `Bearer ${n.value}`);
+    },
+    async onRequestError({ error: e }) {
+      console.log(e.message);
+    },
+    async onResponseError({ response: e }) {
+      console.log(e._data.message);
+    },
+    ...t
+  }, { data: r, pending: s, error: c, execute: u } = await useFetch(o, a);
+  return {
+    data: r,
+    pending: s,
+    error: c,
+    execute: u
+  };
 };
 export {
-  o as formatDate,
-  a as useHttp
+  i as formatDate,
+  d as useHttp
 };
