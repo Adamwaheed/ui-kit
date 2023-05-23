dfe0c898da85fdf407e5a17d3923b8f6ae008890
Ahmed Naxeem <anazeem@gmail.com>
2023-05-23 15:13:35 +0500

    IT-409 IT-410 PoCardTabs documentation added

---
+++ src/components/PoCardTabs/PoCardTabs.stories.js
@@ -0,0 +1,83 @@
+// YourComponent.stories.js
+
+import {
+	BanknotesIcon as BanknotesIconSolid,
+	BriefcaseIcon,
+	MapPinIcon,
+	ScissorsIcon,
+} from "@heroicons/vue/20/solid";
+
+import PoCardTabs from "./PoCardTabs.vue";
+
+import PoCardTabsDocs from "./PoCardTabsDocs.stories.mdx";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "Layout/Card Tabs",
+	component: PoCardTabs,
+	parameters: {
+		docs: {
+			page: PoCardTabsDocs,
+			description: {
+				component: "Card tabs",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#f8fafc" }],
+		},
+	},
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => ({
+	components: { PoCardTabs },
+	setup() {
+		//👇 The args will now be passed down to the template
+		return { args };
+	},
+	template: '<PoCardTabs v-bind="args" />',
+});
+
+export const CardTabs = Template.bind({});
+
+CardTabs.args = {
+	/* 👇 The args you need here will depend on your component */
+	tabs: [
+		{
+			name: "Wage Changes",
+			icon: BanknotesIconSolid,
+			iconColor: "po-fill-green-400",
+			href: "#",
+			current: true,
+			count: 20,
+		},
+		{
+			name: "Terminations",
+			icon: ScissorsIcon,
+			iconColor: "po-fill-red-400",
+			href: "#",
+			current: false,
+			count: 999,
+		},
+		{
+			name: "Location Changes",
+			icon: MapPinIcon,
+			iconColor: "po-fill-orange-400",
+			href: "#",
+			current: false,
+			count: 9,
+		},
+		{
+			name: "Designation Changes",
+			icon: BriefcaseIcon,
+			iconColor: "po-fill-cyan-400",
+			href: "#",
+			current: false,
+		},
+	],
+};
