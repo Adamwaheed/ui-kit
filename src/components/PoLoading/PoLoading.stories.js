862e6c968f87c5799fa564ce80c1866fdfcf452a
Ahmed Naxeem <anazeem@gmail.com>
2023-05-24 16:00:26 +0500

    IT-414 IT-415 loading stories migrated

--- src/components/PoLoading/PoLoading.stories.js
+++ src/components/PoLoading/PoLoading.stories.js
@@ -2,50 +2,50 @@
 
 import PoLoading from "./PoLoading.vue";
 
-import PoLoadingDocs from "./PoLoadingDocs.stories.mdx";
+import PoLoadingDocs from "./PoLoadingDocs.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
-  title: "Components/Loading",
-  component: PoLoading,
-  parameters: {
-    docs: {
-      page: PoLoadingDocs,
-      description: {
-        component: "Loading component.",
-      },
-    },
-    backgrounds: {
-      default: "gray",
-      values: [{ name: "gray", value: "#f8fafc" }],
-    },
-  },
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "Components/Loading",
+	component: PoLoading,
+	parameters: {
+		docs: {
+			page: PoLoadingDocs,
+			description: {
+				component: "Loading component.",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#f8fafc" }],
+		},
+	},
 };
 
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-  components: { PoLoading },
-  setup() {
-    //👇 The args will now be passed down to the template
-    return { args };
-  },
-  template: '<PoLoading v-bind="args" />',
+	components: { PoLoading },
+	setup() {
+		//👇 The args will now be passed down to the template
+		return { args };
+	},
+	template: '<PoLoading v-bind="args" />',
 });
 
 export const Loading = Template.bind();
 export const WithLabel = Template.bind();
 
 Loading.args = {
-  /* 👇 The args you need here will depend on your component */
-  show: true,
+	/* 👇 The args you need here will depend on your component */
+	show: true,
 };
 
 WithLabel.args = {
-  /* 👇 The args you need here will depend on your component */
-  show: true,
-  label: "Please wait",
+	/* 👇 The args you need here will depend on your component */
+	show: true,
+	label: "Please wait",
 };
