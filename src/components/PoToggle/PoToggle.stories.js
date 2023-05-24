c3abeb27f7c6d05fc9b8914f049a93887233d59d
Ahmed Naxeem <anazeem@gmail.com>
2023-05-24 14:53:14 +0500

    IT-414 IT-415 toggle stories migrated

--- src/components/PoToggle/PoToggle.stories.js
+++ src/components/PoToggle/PoToggle.stories.js
@@ -1,45 +1,46 @@
 // YourComponent.stories.js
 
-import PoToggle from './PoToggle.vue';
-import PoCard from '../PoCard/PoCard.vue';
+import PoToggle from "./PoToggle.vue";
+
+import PoToggleDocs from "./PoToggleDocs.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Form/Toggle',
-  component: PoToggle,
-  parameters: {
-    docs: {
-      description: {
-        component: 'Toggle button with label'
-      }
-    },
-    backgrounds: {
-      default: 'gray',
-      values: [
-        { name: 'gray', value: '#f8fafc' },
-      ],
-    },
-  },
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "Form/Toggle",
+	component: PoToggle,
+	parameters: {
+		docs: {
+			page: PoToggleDocs,
+			description: {
+				component: "Toggle button with label",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#f8fafc" }],
+		},
+	},
 };
 
+import { ref } from "vue";
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-  components: { PoToggle, PoCard },
-  setup() {
-    //👇 The args will now be passed down to the template
-    return { args };
-  },
-  template: '<PoCard class="po-mt-5 po-p-5 po-max-w-md"> <template v-slot:content><PoToggle v-bind="args" /></template></PoCard>',
+	components: { PoToggle },
+	setup() {
+		let toggleVal = ref(false);
+		//👇 The args will now be passed down to the template
+		return { args, toggleVal };
+	},
+	template:
+		'<PoToggle label="Toggle me" v-model="toggleVal" /><br /><br /><span class="po-text-sm po-text-slate-500">Toggle  value: {{ toggleVal }}</span>',
 });
 
 export const Toggle = Template.bind({});
 
 Toggle.args = {
-  /* 👇 The args you need here will depend on your component */
-  label: "Click me",
-  type: ''
+	/* 👇 The args you need here will depend on your component */
 };
