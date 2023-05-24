e791cb43f937d5b8789d6f44101b65ab7b487b75
Ahmed Naxeem <anazeem@gmail.com>
2023-05-24 09:05:29 +0500

    IT-414 IT-415 Content area stories migrated

--- src/components/PoContentArea/PoContentArea.stories.js
+++ src/components/PoContentArea/PoContentArea.stories.js
@@ -1,41 +1,39 @@
 // YourComponent.stories.js
 
-import PoContentArea from './PoContentArea.vue';
+import PoContentArea from "./PoContentArea.vue";
 
-import PoContentAreaDocs from './PoContentAreaDocs.stories.mdx';
+import PoContentAreaDocs from "./PoContentAreaDocs.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Layout/Content Area',
-  component: PoContentArea,
-  parameters: {
-    docs: {
-      page: PoContentAreaDocs,
-      description: {
-          component: 'This is the wrapper for every page.'
-      }
-    },
-    backgrounds: {
-      default: 'gray',
-      values: [
-        { name: 'gray', value: '#f8fafc' },
-      ],
-    },
-  },
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "Layout/Content Area",
+	component: PoContentArea,
+	parameters: {
+		docs: {
+			page: PoContentAreaDocs,
+			description: {
+				component: "This is the wrapper for every page.",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#f8fafc" }],
+		},
+	},
 };
 
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-  components: { PoContentArea },
-  setup() {
-    //👇 The args will now be passed down to the template
-    return { args };
-  },
-  template: '<PoContentArea>...</PoContentArea>',
+	components: { PoContentArea },
+	setup() {
+		//👇 The args will now be passed down to the template
+		return { args };
+	},
+	template: "<PoContentArea>...</PoContentArea>",
 });
 
 export const ContentArea = Template.bind();
