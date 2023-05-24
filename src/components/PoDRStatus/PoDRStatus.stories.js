4ac127cc5066ed2cdcf0e6ee27257802f02423b3
Ahmed Naxeem <anazeem@gmail.com>
2023-05-24 16:37:12 +0500

    IT-414 IT-415 drs status stories migrated

--- src/components/PoDRStatus/PoDRStatus.stories.js
+++ src/components/PoDRStatus/PoDRStatus.stories.js
@@ -2,38 +2,38 @@
 
 import PoDRStatus from "./PoDRStatus.vue";
 
-import PoDRStatusDocs from "./PoDRStatusDocs.stories.mdx";
+import PoDRStatusDocs from "./PoDRStatusDocs.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
-  title: "DRS/DRStatus",
-  component: PoDRStatus,
-  parameters: {
-    docs: {
-      page: PoDRStatusDocs,
-      description: {
-        component: "DRS Status component, to display death record status",
-      },
-    },
-    backgrounds: {
-      default: "gray",
-      values: [{ name: "gray", value: "#fff" }],
-    },
-  },
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "DRS/DRStatus",
+	component: PoDRStatus,
+	parameters: {
+		docs: {
+			page: PoDRStatusDocs,
+			description: {
+				component: "DRS Status component, to display death record status",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#fff" }],
+		},
+	},
 };
 
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-  components: { PoDRStatus },
-  setup() {
-    //👇 The args will now be passed down to the template
-    return { args };
-  },
-  template: '<PoDRStatus v-bind="args"/>',
+	components: { PoDRStatus },
+	setup() {
+		//👇 The args will now be passed down to the template
+		return { args };
+	},
+	template: '<PoDRStatus v-bind="args"/>',
 });
 
 export const NotDead = Template.bind();
@@ -42,78 +42,78 @@ export const Record = Template.bind();
 export const Request = Template.bind();
 
 NotDead.args = {
-  /* 👇 The args you need here will depend on your component */
-  member: {
-    name: "Mohamed Ashraf Majdhee",
-    identifier: "A166597",
-    dob: "1989-09-07T00:00:00.000Z",
-  },
-  record: {
-    id: 7,
-    institution: "Maldives Pension Administration Office",
-    date_of_death: "2023-02-16T00:00:00.000Z",
-  },
-  request: {
-    id: 16,
-    type: "Dispute",
-    type_id: 2,
-    state: "Pending",
-    state_id: 1,
-    dispute_type: "Not Dead",
-    dispute_type_id: 1,
-    institution: "Maldives Pension Administration Office",
-  },
+	/* 👇 The args you need here will depend on your component */
+	member: {
+		name: "Mohamed Ashraf Majdhee",
+		identifier: "A166597",
+		dob: "1989-09-07T00:00:00.000Z",
+	},
+	record: {
+		id: 7,
+		institution: "Maldives Pension Administration Office",
+		date_of_death: "2023-02-16T00:00:00.000Z",
+	},
+	request: {
+		id: 16,
+		type: "Dispute",
+		type_id: 2,
+		state: "Pending",
+		state_id: 1,
+		dispute_type: "Not Dead",
+		dispute_type_id: 1,
+		institution: "Maldives Pension Administration Office",
+	},
 };
 WrongDeath.args = {
-  /* 👇 The args you need here will depend on your component */
-  member: {
-    name: "Mohamed Ashraf Majdhee",
-    identifier: "A166597",
-    dob: "1989-09-07T00:00:00.000Z",
-  },
-  record: {
-    id: 7,
-    institution: "Maldives Pension Administration Office",
-    date_of_death: "2023-02-16T00:00:00.000Z",
-  },
-  request: {
-    id: 17,
-    type: "Dispute",
-    type_id: 2,
-    state: "Pending",
-    state_id: 1,
-    dispute_type: "Wrong Date",
-    dispute_type_id: 1,
-    institution: "Maldives Pension Administration Office",
-    date_of_death: "2023-02-16T00:00:00.000Z",
-  },
+	/* 👇 The args you need here will depend on your component */
+	member: {
+		name: "Mohamed Ashraf Majdhee",
+		identifier: "A166597",
+		dob: "1989-09-07T00:00:00.000Z",
+	},
+	record: {
+		id: 7,
+		institution: "Maldives Pension Administration Office",
+		date_of_death: "2023-02-16T00:00:00.000Z",
+	},
+	request: {
+		id: 17,
+		type: "Dispute",
+		type_id: 2,
+		state: "Pending",
+		state_id: 1,
+		dispute_type: "Wrong Date",
+		dispute_type_id: 1,
+		institution: "Maldives Pension Administration Office",
+		date_of_death: "2023-02-16T00:00:00.000Z",
+	},
 };
 Record.args = {
-  /* 👇 The args you need here will depend on your component */
-  member: {
-    name: "Mohamed Ashraf Majdhee",
-    identifier: "A166597",
-    dob: "1989-09-07T00:00:00.000Z",
-  },
-  record: {
-    id: 7,
-    institution: "Maldives Pension Administration Office",
-    date_of_death: "2023-02-16T00:00:00.000Z",
-  },
+	/* 👇 The args you need here will depend on your component */
+	member: {
+		name: "Mohamed Ashraf Majdhee",
+		identifier: "A166597",
+		dob: "1989-09-07T00:00:00.000Z",
+	},
+	record: {
+		id: 7,
+		institution: "Maldives Pension Administration Office",
+		date_of_death: "2023-02-16T00:00:00.000Z",
+	},
 };
 Request.args = {
-  /* 👇 The args you need here will depend on your component */
-  member: {
-    name: "Mohamed Ashraf Majdhee",
-    identifier: "A166597",
-    dob: "1989-09-07T00:00:00.000Z",
-  },
-  request: {
-    id: 17,
-    type: "New",
-    type_id: 1,
-    state: "Pending",
-    state_id: 1,
-    institution: "Maldives Pension Administration Office",
-  },
+	/* 👇 The args you need here will depend on your component */
+	member: {
+		name: "Mohamed Ashraf Majdhee",
+		identifier: "A166597",
+		dob: "1989-09-07T00:00:00.000Z",
+	},
+	request: {
+		id: 17,
+		type: "New",
+		type_id: 1,
+		state: "Pending",
+		state_id: 1,
+		institution: "Maldives Pension Administration Office",
+	},
 };
