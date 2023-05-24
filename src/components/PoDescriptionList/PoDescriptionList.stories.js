42031aaf4f156c19e15cc2da584a466295819e88
Ahmed Naxeem <anazeem@gmail.com>
2023-05-24 09:50:23 +0500

    IT-414 IT-415 Description list stories migrated

--- src/components/PoDescriptionList/PoDescriptionList.stories.js
+++ src/components/PoDescriptionList/PoDescriptionList.stories.js
@@ -1,42 +1,42 @@
 // YourComponent.stories.js
 
-import PoDescriptionList from './PoDescriptionList.vue';
-import PoCard from '../PoCard/PoCard.vue';
+import PoDescriptionList from "./PoDescriptionList.vue";
+import PoCard from "../PoCard/PoCard.vue";
 
-import PoDescriptionListDocs from './PoDescriptionListDocs.stories.mdx';
+import PoDescriptionListDocs from "./PoDescriptionListDocs.mdx";
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Content/Description List',
-  component: PoDescriptionList,
-  parameters: {
-    docs: {
-      page: PoDescriptionListDocs,
-      description: {
-        component: 'Description list component is useful when displaying content in an easy to read way.'
-      }
-    },
-    backgrounds: {
-      default: 'gray',
-      values: [
-        { name: 'gray', value: '#f8fafc' },
-      ],
-    },
-  },
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "Content/Description List",
+	component: PoDescriptionList,
+	parameters: {
+		docs: {
+			page: PoDescriptionListDocs,
+			description: {
+				component:
+					"Description list component is useful when displaying content in an easy to read way.",
+			},
+		},
+		backgrounds: {
+			default: "gray",
+			values: [{ name: "gray", value: "#f8fafc" }],
+		},
+	},
 };
 
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-  components: { PoDescriptionList, PoCard },
-  setup() {
-    //👇 The args will now be passed down to the template
-    return { args };
-  },
-  template: '<PoCard class="po-mt-5 po-p-5"> <template v-slot:content> <PoDescriptionList v-bind="args" /></template></PoCard>',
+	components: { PoDescriptionList, PoCard },
+	setup() {
+		//👇 The args will now be passed down to the template
+		return { args };
+	},
+	template:
+		'<PoCard class="po-mt-5 po-p-5"> <template v-slot:content> <PoDescriptionList v-bind="args" /></template></PoCard>',
 });
 
 export const List = Template.bind({});
@@ -44,65 +44,65 @@ export const WithAction = Template.bind({});
 export const Striped = Template.bind({});
 
 List.args = {
-  /* 👇 The args you need here will depend on your component */
-  items: [
-    {
-      title: "Full name",
-      description: "Saleem Ahmed Doe",
-    },
-    {
-        title: "Address",
-        description: "House of Sal, Medhu St, Malé",
-    },
-    {
-        title: "Dob",
-        description: "1972-02-05",
-        action: "Update",
-    }
-    ]
+	/* 👇 The args you need here will depend on your component */
+	items: [
+		{
+			title: "Full name",
+			description: "Saleem Ahmed Doe",
+		},
+		{
+			title: "Address",
+			description: "House of Sal, Medhu St, Malé",
+		},
+		{
+			title: "Dob",
+			description: "1972-02-05",
+			action: "Update",
+		},
+	],
 };
 
 WithAction.args = {
-  /* 👇 The args you need here will depend on your component */
-  items: [
-    {
-      title: 'Full name',
-      description: 'Saleem Ahmed Doe',
-      action: 'Edit'
-    },
-    {
-      title: 'Address',
-      description: 'House of Sal, Medhu St, Malé',
-      action: 'Open map'
-    },
-    {
-      title: 'Dob',
-      description: '1972-02-05',
-      action: 'Update'
-    }
-    ]
+	/* 👇 The args you need here will depend on your component */
+	items: [
+		{
+			title: "Full name",
+			description: "Saleem Ahmed Doe",
+			action: "Edit",
+		},
+		{
+			title: "Address",
+			description: "House of Sal, Medhu St, Malé",
+			action: "Open map",
+		},
+		{
+			title: "Dob",
+			description: "1972-02-05",
+			action: "Update",
+		},
+	],
 };
 
 Striped.args = {
-    /* 👇 The args you need here will depend on your component */
-    items: [
-      {
-        title: 'Full name',
-        description: 'Saleem Ahmed Doe',
-        action: ''
-      },
-      {
-        title: 'Address',
-        description: 'House of Sal, Medhu St, Malé'
-      },
-      {
-        title: 'Dob',
-        description: '1972-02-05'
-      },
-      {
-        title: 'Position',
-        description: 'Crawler'
-      }
-      ],
-      striped: true
-  };
+	/* 👇 The args you need here will depend on your component */
+	items: [
+		{
+			title: "Full name",
+			description: "Saleem Ahmed Doe",
+			action: "",
+		},
+		{
+			title: "Address",
+			description: "House of Sal, Medhu St, Malé",
+		},
+		{
+			title: "Dob",
+			description: "1972-02-05",
+		},
+		{
+			title: "Position",
+			description: "Crawler",
+		},
+	],
+	striped: true,
+};
