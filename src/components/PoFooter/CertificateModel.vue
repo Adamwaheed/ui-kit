05d4a1ea02c8c7bc6aab772588a011c2a1561bd1
Ahmed Naxeem <anazeem@gmail.com>
2023-05-31 10:27:58 +0500

    IT-472 IT-508 decided against using images inside ui kit as it increases the build file size of the package, so certs needs to be linked from elsewhere

---
+++ src/components/PoFooter/CertificateModel.vue
@@ -0,0 +1,40 @@
+<template>
+	<component
+		:is="logo"
+		@click.prevent="showCertModal = true"
+		class="po-cursor-pointer"
+		role="button"
+	/>
+	<PoModal
+		:show="showCertModal"
+		@modal-closed="showCertModal = false"
+		:modal-title="title"
+		modal-width="sm:po-max-w-4xl"
+		id="change-log-modal"
+	>
+		<template v-slot:content>
+			<div class="-po-m-5">
+				<img :src="src" :alt="title" />
+			</div>
+		</template>
+	</PoModal>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import PoModal from "../PoModal/PoModal.vue";
+
+defineProps({
+	src: {
+		type: String,
+		default: "",
+	},
+	title: {
+		type: String,
+		default: "",
+	},
+	logo: null,
+});
+
+const showCertModal = ref(false);
+</script>
