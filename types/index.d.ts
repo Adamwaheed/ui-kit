7ea1dfe3f34b0bb79ba64fe664d282050c207a3e
Ahmed Naxeem <anazeem@gmail.com>
2023-07-14 18:16:54 +0500

    IT-1391 types folder back in

---
+++ types/index.d.ts
@@ -0,0 +1,164 @@
+import { DefineComponent } from "vue";
+import PoTopBar from "../src/components/PoTopBar/PoTopBar.vue";
+import PoAppIconProps from "../src/components/PoAppIcon/PoAppIcon.vue";
+import PoSearchBarProps from "../src/components/PoSearchBar/PoSearchBar.vue";
+import PoAppTrayProps from "../src/components/PoAppTray/PoAppTray.vue";
+import PoNotificationHubProps from "../src/components/PoNotificationHub/PoNotificationHub.vue";
+import PoProfileSwitcherProps from "../src/components/PoProfileSwitcher/PoProfileSwitcher.vue";
+import PoSidebarDrawerProps from "../src/components/PoSidebarDrawer/PoSidebarDrawer.vue";
+import PoActionBarProps from "../src/components/PoActionBar/PoActionBar.vue";
+import PoCardProps from "../src/components/PoCard/PoCard.vue";
+import PoCardSearchProps from "../src/components/PoCardSearch/PoCardSearch.vue";
+import PoPageTitleProps from "../src/components/PoPageTitle/PoPageTitle.vue";
+import PoPaginationProps from "../src/components/PoPagination/PoPagination.vue";
+import PoTableProps from "../src/components/PoTable/PoTable.vue";
+import PoDescriptionListProps from "../src/components/PoDescriptionList/PoDescriptionList.vue";
+import PoInputFieldProps from "../src/components/PoInputField/PoInputField.vue";
+import PoInputFileProps from "../src/components/PoInputFile/PoInputFile.vue";
+import PoSelectFieldProps from "../src/components/PoSelectField/PoSelectField.vue";
+import PoButtonProps from "../src/components/PoButton/PoButton.vue";
+import PoModalProps from "../src/components/PoModal/PoModal.vue";
+import PoToggleProps from "../src/components/PoToggle/PoToggle.vue";
+import PoSectionMenuProps from "../src/components/PoSectionMenu/PoSectionMenu.vue";
+import PoCheckboxProps from "../src/components/PoCheckbox/PoCheckbox.vue";
+import PoTextareaProps from "../src/components/PoTextarea/PoTextarea.vue";
+import PoFooterProps from "../src/components/PoFooter/PoFooter.vue";
+import PoFormStatusMessageProps from "../src/components/PoFormStatusMessage/PoFormStatusMessage.vue";
+import PoRadioInputProps from "../src/components/PoRadioInput/PoRadioInput.vue";
+import PoStatsBlockProps from "../src/components/PoStatsBlock/PoStatsBlock.vue";
+import PoChatLogItemProps from "../src/components/PoChatLogItem/PoChatLogItem.vue";
+import PoNotificationProps from "../src/components/PoNotification/PoNotification.vue";
+import PoSlideoverProps from "../src/components/PoSlideover/PoSlideover.vue";
+import PoCommandPaletteProps from "../src/components/PoCommandPalette/PoCommandPalette.vue";
+import PoAlertProps from "../src/components/PoAlert/PoAlert.vue";
+import PoLogsProps from "../src/components/PoLogs/PoLogs.vue";
+import PoDownloadFileListProps from "../src/components/PoDownloadFileList/PoDownloadFileList.vue";
+import PoContentAreaProps from "../src/components/PoContentArea/PoContentArea.vue";
+import PoLoadingProps from "../src/components/PoLoading/PoLoading.vue";
+import PoConsentProps from "../src/components/PoConsent/PoConsent.vue";
+import PoCardTabsProps from "../src/components/PoCardTabs/PoCardTabs.vue";
+import PoTabsProps from "../src/components/PoTabs/PoTabs.vue";
+import PoEmptyProps from "../src/components/PoEmpty/PoEmpty.vue";
+import PoDRStatusProps from "../src/components/PoDRStatus/PoDRStatus.vue";
+import PoTableActionProps from "../src/components/PoTableAction/PoTableAction.vue";
+import RufiyaaProps from "../src/components/Rufiyaa/Rufiyaa.vue";
+import PoHeadingProps from "../src/components/PoHeading/PoHeading.vue";
+import PoMultiSelectProps from "../src/components/PoMultiSelect/PoMultiSelect.vue";
+import PoTimelineProps from "../src/components/PoTimeline/PoTimeline.vue";
+import PoTableCheckboxProps from "../src/components/PoTableCheckbox/PoTableCheckbox.vue";
+import PoToastProps from "../src/components/PoToast/PoToast.vue";
+import PoSelectApiProps from "../src/components/PoSelectApi/PoSelectApi.vue";
+import PoMessageProps from "../src/components/PoMessage/PoMessage.vue";
+import PoPageStatsProps from "../src/components/PoPageStats/PoPageStats.vue";
+import PoTooltipProps from "../src/components/PoTooltip/PoTooltip.vue";
+import PoUploadProps from "../src/components/PoUpload/PoUpload.vue";
+
+declare module "./index" {
+	const PoTopBar: DefineComponent<{}, {}, any>;
+	const PoAppIcon: DefineComponent<{}, {}, any>;
+	const PoSearchBar: DefineComponent<{}, {}, any>;
+	const PoAppTray: DefineComponent<{}, {}, any>;
+	const PoNotificationHub: DefineComponent<{}, {}, any>;
+	const PoProfileSwitcher: DefineComponent<{}, {}, any>;
+	const PoSidebarDrawer: DefineComponent<{}, {}, any>;
+	const PoActionBar: DefineComponent<{}, {}, any>;
+	const PoCard: DefineComponent<{}, {}, any>;
+	const PoCardSearch: DefineComponent<{}, {}, any>;
+	const PoPageTitle: DefineComponent<{}, {}, any>;
+	const PoPagination: DefineComponent<{}, {}, any>;
+	const PoTable: DefineComponent<{}, {}, any>;
+	const PoDescriptionList: DefineComponent<{}, {}, any>;
+	const PoInputField: DefineComponent<{}, {}, any>;
+	const PoInputFile: DefineComponent<{}, {}, any>;
+	const PoSelectField: DefineComponent<{}, {}, any>;
+	const PoButton: DefineComponent<{}, {}, any>;
+	const PoModal: DefineComponent<{}, {}, any>;
+	const PoToggle: DefineComponent<{}, {}, any>;
+	const PoSectionMenu: DefineComponent<{}, {}, any>;
+	const PoCheckbox: DefineComponent<{}, {}, any>;
+	const PoTextarea: DefineComponent<{}, {}, any>;
+	const PoFooter: DefineComponent<{}, {}, any>;
+	const PoFormStatusMessage: DefineComponent<{}, {}, any>;
+	const PoRadioInput: DefineComponent<{}, {}, any>;
+	const PoStatsBlock: DefineComponent<{}, {}, any>;
+	const PoChatLogItem: DefineComponent<{}, {}, any>;
+	const PoNotification: DefineComponent<{}, {}, any>;
+	const PoSlideover: DefineComponent<{}, {}, any>;
+	const PoCommandPalette: DefineComponent<{}, {}, any>;
+	const PoAlert: DefineComponent<{}, {}, any>;
+	const PoLogs: DefineComponent<{}, {}, any>;
+	const PoDownloadFileList: DefineComponent<{}, {}, any>;
+	const PoContentArea: DefineComponent<{}, {}, any>;
+	const PoLoading: DefineComponent<{}, {}, any>;
+	const PoConsent: DefineComponent<{}, {}, any>;
+	const PoCardTabs: DefineComponent<{}, {}, any>;
+	const PoTabs: DefineComponent<{}, {}, any>;
+	const PoEmpty: DefineComponent<{}, {}, any>;
+	const PoDRStatus: DefineComponent<{}, {}, any>;
+	const PoTableAction: DefineComponent<{}, {}, any>;
+	const Rufiyaa: DefineComponent<RufiyaaProps>;
+	const PoHeading: DefineComponent<{}, {}, any>;
+	const PoMultiSelect: DefineComponent<{}, {}, any>;
+	const PoTimeline: DefineComponent<{}, {}, any>;
+	const PoTableCheckbox: DefineComponent<{}, {}, any>;
+	const PoToast: DefineComponent<{}, {}, any>;
+	const PoSelectApi: DefineComponent<{}, {}, any>;
+	const PoMessage: DefineComponent<{}, {}, any>;
+	const PoPageStats: DefineComponent<{}, {}, any>;
+	const PoTooltip: DefineComponent<{}, {}, any>;
+	const PoUpload: DefineComponent<{}, {}, any>;
+	export {
+		PoTopBar,
+		PoAppIcon,
+		PoSearchBar,
+		PoAppTray,
+		PoNotificationHub,
+		PoProfileSwitcher,
+		PoSidebarDrawer,
+		PoActionBar,
+		PoCard,
+		PoCardSearch,
+		PoPageTitle,
+		PoPagination,
+		PoTable,
+		PoDescriptionList,
+		PoInputField,
+		PoInputFile,
+		PoSelectField,
+		PoButton,
+		PoModal,
+		PoToggle,
+		PoSectionMenu,
+		PoCheckbox,
+		PoTextarea,
+		PoFooter,
+		PoFormStatusMessage,
+		PoRadioInput,
+		PoStatsBlock,
+		PoChatLogItem,
+		PoNotification,
+		PoSlideover,
+		PoCommandPalette,
+		PoAlert,
+		PoLogs,
+		PoDownloadFileList,
+		PoContentArea,
+		PoLoading,
+		PoConsent,
+		PoCardTabs,
+		PoTabs,
+		PoEmpty,
+		PoDRStatus,
+		PoTableAction,
+		PoHeading,
+		PoMultiSelect,
+		PoTimeline,
+		PoTableCheckbox,
+		PoToast,
+		PoSelectApi,
+		PoMessage,
+		PoPageStats,
+		PoTooltip,
+		PoUpload,
+	};
+}
