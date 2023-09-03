518c563001ce37523916dcd6fbaa36f7b4da1212
Ahmed Naxeem <ahmed.nazeem@pension.gov.mv>
2023-09-03 05:52:51 +0500

    updated status pill type descripton file

--- src/components/PoStatusPill/PoStatusPill.d.ts
+++ src/components/PoStatusPill/PoStatusPill.d.ts
@@ -3,8 +3,11 @@ import type { FunctionalComponent } from "../../../types/Heroicon";
 
 export declare const PoStatusPill: DefineComponent<
 	{
-		icon: FunctionalComponent | null;
+		icon: FunctionalComponent;
 		label: string;
+		color?: "green" | "blue" | "red";
+		customIconColor?: string | undefined;
+		customBgColor?: string | undefined;
 	},
 	{},
 	{},
