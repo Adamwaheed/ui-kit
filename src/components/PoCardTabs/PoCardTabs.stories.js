8585ea00fa315aea2e59315d00872e312cb94076
Ahmed Naxeem <ahmed.nazeem@pension.gov.mv>
2023-10-04 10:45:10 +0500

    docs updated

--- src/components/PoCardTabs/PoCardTabs.stories.js
+++ src/components/PoCardTabs/PoCardTabs.stories.js
@@ -8,6 +8,7 @@ import {
 } from "@heroicons/vue/20/solid";
 
 import PoCardTabs from "./PoCardTabs.vue";
+import PoCard from "../PoCard/PoCard.vue";
 
 import PoCardTabsDocs from "./PoCardTabsDocs.mdx";
 
@@ -35,54 +36,53 @@ export default {
 
 //👇 We create a “template” of how args map to rendering
 const Template = (args) => ({
-	components: { PoCardTabs },
+	components: { PoCardTabs, PoCard },
 	setup() {
+		const tabs = [
+			{
+				name: "Wage Changes",
+				icon: BanknotesIconSolid,
+				iconColor: "po-fill-green-400",
+				href: "#",
+				current: true,
+				count: 20,
+			},
+			{
+				name: "Terminations",
+				icon: ScissorsIcon,
+				iconColor: "po-fill-red-400",
+				href: "#",
+				current: false,
+				count: 999,
+			},
+			{
+				name: "Location Changes",
+				icon: MapPinIcon,
+				iconColor: "po-fill-orange-400",
+				href: "#",
+				current: false,
+				count: 9,
+			},
+			{
+				name: "Designation Changes",
+				icon: BriefcaseIcon,
+				iconColor: "po-fill-cyan-400",
+				href: "#",
+				current: false,
+			},
+		];
 		function handleButtonClick(clickedTab) {
 			args.tabs.forEach((tab) => {
 				tab.current = tab === clickedTab; // Set 'current' to true for the clicked tab, false for others
 			});
 		}
 		//👇 The args will now be passed down to the template
-		return { args, handleButtonClick };
+		return { args, handleButtonClick, tabs };
 	},
-	template: '<PoCardTabs v-bind="args" @button-click="handleButtonClick" />',
+	template:
+		'<PoCard><PoCardTabs :tabs="tabs" @button-click="handleButtonClick" /><div class="po-p-5">...</div></PoCard>',
 });
 
 export const CardTabs = Template.bind({});
 
-CardTabs.args = {
-	/* 👇 The args you need here will depend on your component */
-	tabs: [
-		{
-			name: "Wage Changes",
-			icon: BanknotesIconSolid,
-			iconColor: "po-fill-green-400",
-			href: "#",
-			current: true,
-			count: 20,
-		},
-		{
-			name: "Terminations",
-			icon: ScissorsIcon,
-			iconColor: "po-fill-red-400",
-			href: "#",
-			current: false,
-			count: 999,
-		},
-		{
-			name: "Location Changes",
-			icon: MapPinIcon,
-			iconColor: "po-fill-orange-400",
-			href: "#",
-			current: false,
-			count: 9,
-		},
-		{
-			name: "Designation Changes",
-			icon: BriefcaseIcon,
-			iconColor: "po-fill-cyan-400",
-			href: "#",
-			current: false,
-		},
-	],
-};
+CardTabs.args = {};
